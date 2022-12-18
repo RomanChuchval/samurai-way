ca9defedaa0ac70d537b057df2fba5c1fe1d7582
Roman Chuchval <rchuchval@gmail.com>
2022-12-19 00:54:36 +0300

    Components structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import Header from './Components/Header';
+import Navbar from "./Components/Navbar";
+import Profile from './Components/Profile'
 const App = () => {
     return (
-        <div>
+        <div className='app-wrapper'>
             <Header/>
-            <Technologies/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
-}
-
-const Header = () => {
-    return (
-        <div>
-            <a href="#">Home </a>
-            <a href="#">News Feed </a>
-            <a href="#">Messages</a>
-        </div>
-    )
-}
-
-const Technologies = () => {
-    return (
-        <div>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JS</li>
-                <li>REACT</li>
-            </ul>
-        </div>
-    )
 }
 
 export default App;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://seeklogo.com/images/C/corporate-company-logo-BAE6B43FF7-seeklogo.com.png'/>
+
+        </header>
+
+    )
+}
+
+export default Header
