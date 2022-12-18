a2833498e5bd4f2563565ec382660b2accb16f4c
Roman Chuchval <rchuchval@gmail.com>
2022-12-18 19:10:39 +0300

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home </a>
+            <a href="#">News Feed </a>
+            <a href="#">Messages</a>
+        </div>
+    )
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>REACT</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
